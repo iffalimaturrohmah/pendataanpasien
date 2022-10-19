7fed8cf1f9bf56b019f282e7fb0b1788d3e74f7f
iffalimaturrohmah <109191221+iffalimaturrohmah@users.noreply.github.com>
2022-10-19 11:34:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftaran Pasien Rumah Sakit</title>
+</head>
+<center>
+<body>
+   <h2>RUMAH SAKIT</h2>
+   <h2> SEHAT WARAS SAYUNG</h2> 
+</body>
+<ul>
+    <h4> <a href = "profil.php">Profil</a></h4>
+    <h4> <a href = "pasien.php">Liat Pasien</a></h4>
+    <h4> <a href = "tambah_pasien.php">Tambah Pasien</a></h4>
+    <h4> <a href = "fasilitas.php">Fasilitas</a></h4>
+</html>
